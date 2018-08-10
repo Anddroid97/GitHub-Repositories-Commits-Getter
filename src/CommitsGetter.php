147523a8b7c8f80345f869a781874dfab5fd1350
Andrew <litkovskiy97@gmail.com>
2018-08-10 15:19:48 +0200

    Add Functionallity

--- src/CommitsGetter.php
+++ src/CommitsGetter.php
@@ -1,6 +1,49 @@
 <?php
 
-class CommitsGetter
+namespace Anddroid97\CommitsGetter;
+
+use Anddroid97\CommitsGetter\CommitsGetterInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Anddroid97\CommitsGetter\TestModel\CommitInfo;
+
+class CommitsGetter implements CommitsGetterInterface
 {
-g
+    private $repositoryName;
+
+    public function __construct(string $repositoryName)
+    {
+        $this->repositoryName = $repositoryName;
+    }
+
+    public function fetchCommitsList(): ?array
+    {
+        $client = new Client();
+        $endpoint = 'https://api.github.com/repos/' . $this->repositoryName . '/commits';
+
+        try {
+            $response = $client->request('GET', $endpoint);
+        } catch (RequestException $e) {
+            if ($e->getResponse()->getStatusCode() === 404) {
+                return null;
+            }
+        }
+
+        $json = (string)$response->getBody();
+        $commitsInfo = \json_decode($json, true);
+
+        $filteredCommitsRepository = [];
+
+        //TODO: below you can use your model with setters and set what kind of info you need from commit
+        foreach ($commitsInfo as $commit) {
+            $commitObj = new CommitInfo();
+            $commitObj->setHash($commit['sha']);
+            $commitObj->setAuthor($commit['commit']['author']['name']);
+            $commitObj->setMessage($commit['commit']['message']);
+            $filteredCommitsRepository[] = $commitObj;
+        }
+
+        return $filteredCommitsRepository;
+    }
+
 }
